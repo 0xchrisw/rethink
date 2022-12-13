ff84a22fc1113ed0c1eb420981a60106b99af817
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-13 18:51:48 -0500

    add `data` dir

--- rethink/cli/ephemeral.py
+++ rethink/cli/ephemeral.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 
+import json
+
+
+from pathlib import Path
+
 import rethink.preprocessors as preprocessors
 
 
 def main():
     pdf_path = "data/Elasticsearch - The Definitive Guide.pdf"
-    text = preprocessors.pdf2txt.PDF(pdf_path).result()
-    print(text)
+    # text = preprocessors.pdf2txt.PDF(pdf_path).result()
+    # print(text)
+
+    # output_file = Path(pdf_path).parent / "parsed.json"
+    print(Path(pdf_path).parent / "parsed.json")
 
 
 if __name__ == "__main__":
