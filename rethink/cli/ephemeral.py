d9b8d18aad7aae08d1557f3e4a5e6e97e59b6eba
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-13 19:18:51 -0500

    Update ephemeral.py

--- rethink/cli/ephemeral.py
+++ rethink/cli/ephemeral.py
@@ -15,21 +15,22 @@ import rethink.preprocessors as preprocessors
 model_checkpoint = "distilbert-base-uncased"
 model = TFAutoModelForMaskedLM.from_pretrained(model_checkpoint)
 
-model.summary()
+pdf_path = "data/Elasticsearch - The Definitive Guide.pdf"
+output_file = Path(pdf_path).parent / "parsed.json"
+
+parsed_data = json.loads(output_file.read_text()) if output_file.exists() else None
+
+
+if not parsed_data:
+    parsed_data = preprocessors.pdf2txt.PDF(pdf_path).result()
+    output_file.write_text(json.dumps(parsed_data, indent=2))
+
+# print(parsed_data)
+# print(model.summary())
 
 
 def main():
-    pdf_path = "data/Elasticsearch - The Definitive Guide.pdf"
-    output_file = Path(pdf_path).parent / "parsed.json"
-
-    parsed_data = json.loads(output_file.read_text()) if output_file.exists() else None
-
-    if not parsed_data:
-        parsed_data = preprocessors.pdf2txt.PDF(pdf_path).result()
-        output_file.write_text(json.dumps(parsed_data, indent=2))
-
-    print(parsed_data)
-
+    ...
 
 if __name__ == "__main__":
     main()
