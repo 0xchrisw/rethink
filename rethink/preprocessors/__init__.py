487389b79bf798b961d2ab354962aab885a4b5d6
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-13 19:20:13 -0500

    Update __init__.py

--- rethink/preprocessors/__init__.py
+++ rethink/preprocessors/__init__.py
@@ -1,10 +1,9 @@
-
-
 from . import pdf2txt
 
 class PreProcessor:
     def __init__(self, data):
         self.data = data
+        self._process()
 
     def _process(self):
         raise NotImplementedError
