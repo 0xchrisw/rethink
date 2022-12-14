eca334a59f8cab02798dbd3f3c02a756aa8e6bff
Christopher Woodall <woodall.christopher@gmail.com>
2022-12-13 19:00:35 -0500

    Update ephemeral.py

--- rethink/cli/ephemeral.py
+++ rethink/cli/ephemeral.py
@@ -11,11 +11,15 @@ import rethink.preprocessors as preprocessors
 
 def main():
     pdf_path = "data/Elasticsearch - The Definitive Guide.pdf"
-    # text = preprocessors.pdf2txt.PDF(pdf_path).result()
-    # print(text)
+    output_file = Path(pdf_path).parent / "parsed.json"
 
-    # output_file = Path(pdf_path).parent / "parsed.json"
-    print(Path(pdf_path).parent / "parsed.json")
+    parsed_data = json.loads(output_file.read_text()) if output_file.exists() else None
+
+    if not parsed_data:
+        parsed_data = preprocessors.pdf2txt.PDF(pdf_path).result()
+        output_file.write_text(json.dumps(parsed_data, indent=2))
+
+    print(parsed_data)
 
 
 if __name__ == "__main__":
